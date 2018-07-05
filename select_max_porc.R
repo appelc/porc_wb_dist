2f5f04e535cec4045d5025365a389c6cd20870f8
Your Name <cla236@humboldt.edu>
2018-07-05 15:01:49 -0700

    maxent code using data from 'porc_sdm_prep.R' script

--- select_max_porc.R
+++ select_max_porc.R
@@ -1,118 +1,33 @@
-## Porcupine SDM using occurrence records from Oregon and Washington
+## Porcupine SDM using occurrence records from Oregon, Washington, and northern California
 
-library(rgdal) 
-library(raster)
-library(googlesheets)
-library(prism)
+library(dismo)
+
+## Import dataframe of pres/bg points with predictor values
+
+    cur.data <- read.csv('sdm_data/cur_data_070518.csv')
+    head(cur.data)   
+    cur.data <- cur.data[,-1] #get rid of row index column
+    table(cur.data$pres)
+    sapply(cur.data, class)
 
 
-## 1. LOAD PRESENCE POINTS
-
-    ## occurrence records (OR/WA and northern CA):
-      porc_occur_orwa <- readOGR(dsn = './shapefiles/observations', layer = 'orwa_occur_060618')
-      porc_occur_nca <- readOGR(dsn = './shapefiles/observations', layer = 'nca_occur_013117')
-      porc_occur_nca <- spTransform(porc_occur_nca, CRS('+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0'))
-    
-    ## wood block detections:
-      porc_wb <- readOGR(dsn = './shapefiles/observations', layer = 'porc_wb_pts')  
-      porc_wb <- spTransform(porc_wb, CRS('+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0'))
-    
-    ## combine:
-      porcs <- bind(porc_occur_orwa, porc_occur_nca, porc_wb)
-      
-## 2. GENERATE BACKGROUND POINTS WITHIN STUDY AREA
-  
-    aoi <- readOGR(dsn = './shapefiles/admin', layer = 'OR_WA_NCA_dissolved')
-    aoi@proj4string <- porcs@proj4string
-        
-    bg.pts <- spsample(aoi, n = 10000, type = 'random')
-      
-  
-## 3. COMBINE PRESENCE/BACKGROUND POINTS
-      
-    porc.pts <- as.data.frame(porcs)
-    porc.pts$pres <- rep(1, nrow(porc.pts)) # 1 = presence
-    porc.pts <- porc.pts[,c(353,351:352)] # keep only pres, lon, lat
-    colnames(porc.pts) <- c('pres', 'x', 'y')
-      
-    bg.pts <- as.data.frame(bg.pts)
-    bg.pts$pres <- rep(0, nrow(bg.pts)) # 0 = background
-
-    all.pts <- rbind(porc.pts, bg.pts)
-      head(all.pts)
-      table(all.pts$pres)
-      
-  # convert back to SPDF
-    sp.all.pts <- SpatialPointsDataFrame(data.frame(all.pts$x, all.pts$y), data = all.pts, 
-                                           proj4string = porcs@proj4string)
-      
-  
-## 4. LOAD PREDICTORS & EXTRACT VALUES
-   
-    ## a. distance to water (Euclidean Distance raster calculated from TIGER lines in ArcMap)
-  
-          water <- raster('shapefiles/ORWANCA_riv.tif')
-      
-    ## b. PRISM climate data
-            
-          options(prism.path = './shapefiles/prism')
-          get_prism_normals(type = 'ppt', resolution = '4km', annual = TRUE, keepZip = FALSE)            
-        
-          ppt <- prism_stack(ls_prism_data()[1,1]) ##raster file of data
-            
-          ppt <- crop(ppt, water)
-          water <- resample(water, ppt)          
-          
-    ## c. NLCD data
-            
-        ## I clipped this in ArcMap because the file was really big. 
-        ## Is there a better way to deal with large rasters like this in R?
-          
-          land_cover <- raster('shapefiles/nlcd/nlcd_2011_landcover_clip.img')
-          
-          lc <- crop(land_cover, ppt)   ## need to be same projection   
-          lc <- projectRaster(land_cover, ppt) ## too big. should I re-project in ArcMap too?
-            
-          
-    ## d. fisher population polygons
-          
-          fisher <- readOGR(dsn = './shapefiles/fisher/commondata/usfws_ranges_sep2015', 
-                            layer = 'West_Coast_fisher_populations')
-          fisher <- spTransform(fisher, CRS(''))
-        ## finish
-          
-          plot(fisher)
-  
-    ## extract raster values
-
-      sp.all.pts$water_dist <- extract(water_dist, sp.all.pts)
-      sp.all.pts$ppt_annual <- extract(ppt_annual, sp.all.pts)
-        sp.all.pts$ppt_annual <- as.numeric(sp.all.pts$ppt_annual) ## why is it a matrix?
-      
-     
-  ## simplify dataframe (pres, predictor values)
-      
-      cur.data <- data.frame(sp.all.pts@data[,c(1,4:5)])
-      
-  
- data <- cur.data
-# cor.thresh = 0.5
-# regMult = c(seq(0.5, 3, by = 0.5))
-# classes = "default"
-# testClasses = TRUE
-# out = c("model", "table")
-# anyway = TRUE
-# verbose = TRUE
-# scratchDir=NULL
-# resp = names(data)[1]
-# preds = names(data)[2:ncol(data)]
- path = getwd()
+data <- cur.data
+cor.thresh = 0.5
+regMult = c(seq(0.5, 3, by = 0.5))
+classes = "default"
+testClasses = TRUE
+out = c("model", "table")
+anyway = TRUE
+verbose = TRUE
+scratchDir = NULL
+resp = names(data)[1]
+preds = names(data)[2:ncol(data)]
 
  
 selectMax <- function (data, resp = names(data)[1], preds = names(data)[2:ncol(data)], 
                        cor.thresh = 0.5,
                        regMult = c(seq(0.5, 3, by = 0.5)), classes = "default", testClasses = TRUE, 
-                       out = c('model', 'table'), anyway = TRUE, verbose = TRUE, scratchDir='C:/Users/Cara/Documents/porc_wb_dist/model_temp', 
+                       out = c('model', 'table'), anyway = TRUE, verbose = TRUE, scratchDir = NULL, 
                        path = getwd()) 
 {
   if (class(resp) %in% c("integer", "numeric")) 
@@ -121,12 +36,11 @@ selectMax <- function (data, resp = names(data)[1], preds = names(data)[2:ncol(d
     preds <- names(data)[preds]
   
   scratchDir <- if (is.null(scratchDir)) {
-    base::tempfile(pattern = "/_maxentTempFiles/")
+    base::tempfile(pattern = "_maxentTempFiles/")
   } else {
-    base::tempfile(pattern = "/_maxentTempFiles/", tmpdir = scratchDir)
+    base::tempfile(pattern = "_maxentTempFiles/", tmpdir = scratchDir)
   }
-  dirCreate(scratchDir)  ## Error in selectMax(data) : could not find function "dirCreate" (should be dir.create?)
-  
+  dir.create(scratchDir)  
   
   presentBg <- data[, resp]
   data <- data[, preds, drop = FALSE]
@@ -294,3 +208,13 @@ selectMax <- function (data, resp = names(data)[1], preds = names(data)[2:ncol(d
 
 
 selectMax(data)
+
+
+## got error: could not find function "dirCreate"
+## - changed 'dirCreate' to 'dir.create' on line 43 (is this right/necessary?)
+
+## got error: Error in if (numeric.cors[i, j]) { : missing value where TRUE/FALSE needed
+## -  I think it's because of all the 0s. see:
+
+head(numeric.preds)
+numeric.cors
