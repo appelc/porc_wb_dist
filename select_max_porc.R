31407c27b57b9f597287a910096bbf40ae2738a8
Cara Appel <cla236@humboldt.edu>
2018-10-09 20:06:23 -0700

    updated w fixed cor test

--- select_max_porc.R
+++ select_max_porc.R
@@ -18,7 +18,7 @@ library(rJava)
 
 ## Import dataframe of pres/bg points with predictor values
 
-cur.data <- read.csv('cur_data_recent2012_081518.csv') ## 'cur_data_070518.csv' or 'cur_data_no_wb_071118.csv' or 'cur_data_recent_073018.csv'
+cur.data <- read.csv('cur_data_1981to2010.csv') ## load desired data
 head(cur.data)   
 cur.data <- cur.data[,-1] #get rid of row index column
 table(cur.data$pres)
@@ -40,36 +40,27 @@ sapply(cur.data, function(y) sum(length(which(is.na(y)))))
 cur.data <- cur.data[complete.cases(cur.data),] 
 table(cur.data$pres) #removed 37 rows with NAs
 
-## try with only continuous predictors
+## keep only desired predictors
 
-cur.data <- cur.data[,c(1:6)]
+cur.data <- cur.data[,c(1:3)]
 head(cur.data)
 
 
-## develop candidate models (sets of predictors)
-#  model1 <- which(colnames(cur.data) == 'ppt_800m_clip' | 
-#                    colnames(cur.data) == 'rivers_agg') ## rivers, ppt
-#  model2 <- 
+## set working directory
+  data <- cur.data
+  setwd('C:/Users/cla236/Documents/maxent_porc_100918_no_cat_1981-2010')
+  cor.thresh = 0.5
 
-## try removing the wood block detections to see what the model looks like with the remaining
-## occcurrence points (go back to the 'maxent_prep' code, thin, extract, and export again);
-## same with only points > 1980
-
-
-data <- cur.data
-setwd('C:/Users/cla236/Documents/maxent_no_cat_recent2012_081518')
-
-cor.thresh = 0.5
-regMult = c(seq(0.5, 3, by = 0.5))
-classes = "default"
-testClasses = TRUE
-out = c("model", "table")
-anyway = TRUE
-verbose = FALSE
+#regMult = c(seq(0.5, 3, by = 0.5))
+#classes = "default"
+#testClasses = TRUE
+#out = c("model", "table")
+#anyway = TRUE
+#verbose = FALSE
 #scratchDir = 'C:/Users/cla236/Documents/maxent_no_wb'
-resp = names(data)[1]
-preds = names(data)[2:ncol(data)]
-path = getwd()
+#resp = names(data)[1]
+#preds = names(data)[2:ncol(data)]
+#path = getwd()
 
 
 selectMax <- function (data, resp = names(data)[1], preds = names(data)[2:ncol(data)], 
@@ -98,19 +89,11 @@ selectMax <- function (data, resp = names(data)[1], preds = names(data)[2:ncol(d
   colnames(all.pred.combos) <- names(pred.classes)
   all.pred.combos <- all.pred.combos[-which(rowSums(all.pred.combos) == 0),]
   
-  ## specific candidate models (not just all pred combos):
-  
-  
-  
-  
-  
-  
-  
-  ##
+   ##
   
   # 1. Test correlations b/w numeric vs. numeric
   numeric.preds <- data[,pred.classes %in% "numeric"]
-  numeric.cors <- cor(numeric.preds) > cor.thresh
+  numeric.cors <- abs(cor(numeric.preds)) > cor.thresh ## added 'abs' 10/9/18
   for(i in 1:ncol(numeric.cors)){
     for(j in 1:ncol(numeric.cors)){
       if(i < j){
@@ -264,12 +247,12 @@ selectMax <- function (data, resp = names(data)[1], preds = names(data)[2:ncol(d
 }
 
 
-porc_max_no_cat_recent2012 <- selectMax(data)
+porc_max_1981to2010 <- selectMax(data)
 
-porc_max_no_cat_recent2012$model ## view top model
-head(porc_max_no_cat_recent2012$tuning) ## view model selection table
+porc_max_1981to2010$model ## view top model
+head(porc_max_1981to2010$tuning) ## view model selection table
 
-write.csv(porc_max_no_cat_recent2012$tuning, 'selection_table_081518_no_cat_recent2012.csv')
+write.csv(porc_max_1981to2010$tuning, 'selection_table_100918_no_cat_1981to1010.csv')
 
 numeric.cors ## which predictors were correlated?
 
@@ -278,14 +261,14 @@ numeric.cors ## which predictors were correlated?
 
 ## download the raster stack of predictors on Google Drive (from 'porc_sdm_prep' script)
 
-predictor_stack <- stack('predictors_stack.tif') ## how to get names to import?
+predictor_stack <- stack('C:/Users/cla236/Documents/predictors_stack.tif')
 
 names(predictor_stack) <- c('ppt_800m_clip', 'rivers_agg', 'cancovcon_agg', 'cancovhdw_agg',
                             'tphge3_agg', 'struccond_res', 'vegclass_res', 'nlcd2011_res')
 plot(predictor_stack)
 
-suit_raster <- dismo::predict(porc_max_no_cat_recent2012$model, predictor_stack, progress = 'text')
+suit_raster <- dismo::predict(porc_max_1981to2010$model, predictor_stack, progress = 'text')
 
 plot(suit_raster)
 
-writeRaster(suit_raster, filename = 'porc_suitability_081518_no_cat_recent2012.tif')
+writeRaster(suit_raster, filename = 'porc_suitability_100918_no_cat_1981to2010.tif')
